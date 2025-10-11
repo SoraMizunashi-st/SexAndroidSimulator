b1bb7132d9f83d35266fea8d066a0937d23a25c1
usagi0419 <shuiwukong6@gmail.com>
2025-10-11 09:41:24 +0900

    Initialized Any Settings

--- src/App.cpp
+++ src/App.cpp
@@ -5,5 +5,10 @@ int main()
 {
     std::unique_ptr<SasCore> core = std::make_unique<SasCore>();
 
+    std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++ " << std::endl;
+    std::cout << "This is Test Module SAS Programs " << std::endl;
+    std::cout << "+++++++++++++++++++++++++++++++++++++++++++++++ " << std::endl;
+
+
     return 0;
 }
